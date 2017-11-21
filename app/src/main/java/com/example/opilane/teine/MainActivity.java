5e9c04003f2868ee3ce968e3a86dad420643637c
Opilane TTHK <opilane@tthk.ee>
2017-11-21 11:42:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/opilane/teine/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.opilane.teine;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button Tallinn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Tallinn=(Button)findViewById(R.id.Link);
+
+        Tallinn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://et.wikipedia.org/wiki/Tallinn";
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void pildid(View View, String packageContext) {
+        Intent intent = new Intent(packageContext: MainActivity.this, pildidActivity.class);
+        startActivity(intent);
+    }
+}
